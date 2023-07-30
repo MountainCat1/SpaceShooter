25dbbf3e9b39086e531949a9925131b30eac0647
Mountain Cat <tomasz.banas123@gmail.com>
2023-07-30 15:47:57 +0200

    added stuff :D 💩

---
+++ SpaceShooter/Assets/Scripts/InputManager.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    // public event Action OnUpPressed;
+    //
+    //
+    // private void Update()
+    // {
+    //     if(Input.GetKeyDown())
+    // }
+}
+
+
+// PlayerController <-> Unity API (Input.GetKeyDown()) // shrimple but BAD >:(
+// PlayerController <-> InputManager <-> Unity API (Input.GetKeyDown()) // not shrimple but GOOD ():D 
